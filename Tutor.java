e5e73774b1fccc159d02315ca4dcf19b9f01962a
HoBae <bhb0407@naver.com>
2021-05-12 13:05:47 +0900

    getter, setter를 포함한 자바문법을 확인해보자

---
+++ Tutor.java
@@ -0,0 +1,28 @@
+package com.sparta.week01.prac;
+
+public class Tutor {
+    private String name;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    public Tutor(){}
+
+    public Tutor(String name, String bio){
+        this.name = name;
+        this.bio = bio;
+    }
+}
